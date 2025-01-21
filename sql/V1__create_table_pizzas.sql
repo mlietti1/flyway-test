82e5c1830b6129942481be59f35bfe2bc41ebd24
mlietti1 <mcf.lietti@outlook.com>
2025-01-21 18:35:06 +0100

    datetime fix

--- sql/V1__create_table_pizzas.sql
+++ sql/V1__create_table_pizzas.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS pizzas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
     price DECIMAL(3,2)
-    created_at DATETIME(6)
+    created_at TIMESTAMP
 );
